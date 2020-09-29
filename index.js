eefb616a32ea969e7dbe4822a948c59ae57cde16
Tanyita_bandita <tanja.milutinovic@gmail.com>
2020-09-29 12:04:46 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
@@ -11,114 +11,42 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
-// $("a[href$='more info']").on("click", function() {
-//     $("#dropDown").slideToggle();
-// });
-
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
 
 
-
-
-// $(document).ready(function() {
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
-
-//         // Make sure this.hash has a value before overriding default behavior
-//         if (this.hash !== "") {
-//             // Prevent default anchor click behavior
-//             event.preventDefault();
-
-//             // Store hash
-//             var hash = this.hash;
-
-//             // Using jQuery's animate() method to add smooth page scroll
-//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//             $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 800, function() {
-
-//                 // Add hash (#) to URL when done scrolling (default click behavior)
-//                 window.location.hash = hash;
-//             });
-//         } // End if
-//     });
-// });
-
-
-// $(document).ready(function() {
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
-
-//         // Make sure this.hash has a value before overriding default behavior
-//         if (this.hash !== "") {
-//             // Prevent default anchor click behavior
-//             event.preventDefault();
-
-//             // Store hash
-//             var hash = this.hash;
-
-//             // Using jQuery's animate() method to add smooth page scroll
-//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//             $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 800, function() {
-
-//                 // Add hash (#) to URL when done scrolling (default click behavior)
-//                 window.location.hash = hash;
-//             });
-//         } // End if
-//     });
-// });
-
-
-// Select all links with hashes
 $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
+
+.not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
-        // On-page links
+
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
             location.hostname == this.hostname
         ) {
-            // Figure out element to scroll to
+
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
+
             if (target.length) {
-                // Only prevent default if animation is actually gonna happen
+
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 200, function() {
-                    // Callback after animation
-                    // Must change focus!
+                }, 100, function() {
+
                     var $target = $(target);
                     $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
+                    if ($target.is(":focus")) {
                         return false;
                     } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
+                        $target.attr('tabindex', '-1');
+                        $target.focus();
                     };
                 });
             }
@@ -128,5 +56,5 @@ $('a[href*="#"]')
 
 
 AOS.init({
-    duration: 1200,
+    duration: 1000,
 })
